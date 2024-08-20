9f42a0a12cdebf752511c14fab50d5885945b88a
joão melo <j@melo.plus>
2024-08-20 18:50:32 -0300

    feat: hello world

--- cli/index.js
+++ cli/index.js
@@ -1,2 +1,18 @@
 import { Command } from "commander";
+import { readFileSync } from 'fs';
 
+const packageJson = JSON.parse(readFileSync(new URL('../package.json', import.meta.url)));
+
+const program = new Command();
+
+program
+  .name(packageJson.name)
+  .description(packageJson.description)
+  .version(packageJson.version);
+
+program.command('serve')
+  .action(() => {
+    console.log('serve');
+  });
+
+program.parse();
--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,30 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginVue from "eslint-plugin-vue";
+import stylisticJs from '@stylistic/eslint-plugin-js';
 
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,vue}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  { files: ["**/*.{js,mjs,cjs,vue}"] },
+  { 
+    languageOptions: { 
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: { 
+        ...globals.browser, 
+        ...globals.node 
+      } 
+    } 
+  },
   pluginJs.configs.recommended,
   ...pluginVue.configs["flat/essential"],
+  {
+    plugins: {
+      '@stylistic/js': stylisticJs
+    },
+    rules: {
+      '@stylistic/js/indent': ['error', 2],
+      '@stylistic/js/semi': ["error", "always"]
+    }
+  }
 ];
